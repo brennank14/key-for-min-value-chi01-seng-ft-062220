f87221c55f6effca80b3a1637ebfefd1550ffdd4
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 19:01:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,8 @@ def key_for_min_value(name_hash)
   min = name_hash.first[1]
   name_hash.each do |name, number|
       if number < min
-        puts min = number
+        min = number
       end
   end
+  min
 end
-
-key_for_min_value(hash)
