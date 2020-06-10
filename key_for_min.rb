53e1b5fe90ddba9b92aa6101d2d64b68363ab4c0
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 19:10:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ hash = {}
 def key_for_min_value(name_hash)
   min = name_hash.first[1]
   min_name = name_hash.first[0]
-  
+
   name_hash.each do |name, number|
       if number < min
         min = number
