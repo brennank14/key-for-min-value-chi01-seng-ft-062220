71d08cf11f0baee02f106e583898548d3aa8f930
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 18:59:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  min = name_hash.first[value]
+  min = name_hash.first
   puts min
   #name_hash.each do |name, number|
   #    if number < min
