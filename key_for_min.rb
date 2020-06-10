48b900d53673f79892204511b1a441f700777896
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 19:10:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 hash = {}
 
 def key_for_min_value(name_hash)
@@ -15,3 +14,5 @@ def key_for_min_value(name_hash)
   end
   min_name
 end
+
+key_for_min_value(hash)
